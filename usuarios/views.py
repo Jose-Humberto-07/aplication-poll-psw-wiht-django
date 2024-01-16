019a72bfd9c30afbdd43c606f4b15dcf8855fd5a
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2024-01-16 00:00:11 -0300

    api user finalized

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
 def cadastro(request):
     #return HttpResponse('Olá Mundo, testando mais uma aplicação django')
-    return render(request, 'cadastro.html')
+    print(request.method)
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            return redirect('/usuarios/cadastro')
+        
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            return redirect('/usuarios/cadastro')
+
+        try:
+            user = User.objects.create_user(
+                username=username,
+                password=confirmar_senha,
+            )
+          
+            return redirect('/usuarios/login')
+        except:
+           
+            return redirect('/usuarios/cadastro')
 
