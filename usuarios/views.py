18e09a6e80b0b29dfa299028474c672a75dc33c0
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2024-01-15 22:08:43 -0300

    add base interface

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def cadastro(request):
+    #return HttpResponse('Olá Mundo, testando mais uma aplicação django')
+    return render(request, 'cadastro.html')
+
