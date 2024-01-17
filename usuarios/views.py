c2d67330db7e498968fa376b909f12eac21ec148
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2024-01-16 23:08:33 -0300

    add login project

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 # Create your views here.
 
 def cadastro(request):
@@ -14,11 +16,13 @@ def cadastro(request):
         confirmar_senha = request.POST.get('confirmar_senha')
 
         if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, "Senhas Diferentes, por favor, confirme sua senha novamente.")
             return redirect('/usuarios/cadastro')
         
         user = User.objects.filter(username=username)
 
         if user.exists():
+            messages.add_message(request, constants.ERROR, "Usuário já existe.")
             return redirect('/usuarios/cadastro')
 
         try:
@@ -29,6 +33,11 @@ def cadastro(request):
           
             return redirect('/usuarios/login')
         except:
-           
+            messages.add_message(request, constants.ERROR, "Error interno do servidor.")
             return redirect('/usuarios/cadastro')
 
+
+
+def logar(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
